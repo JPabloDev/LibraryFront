c094ca34306a6b679c30e28342bd603c326ca74e
Juan Quintero <juanvqp@hotmail.com>
2025-05-23 17:50:33 -0500

    initial commit

--- dashboard.js
+++ dashboard.js
@@ -33,7 +33,7 @@ function obtenerDeStorage(nombre) {
 // Mostrar usuarios en el DOM
 async function mostrarUsuarios() {
   //const token = localStorage.getItem("jwtToken");
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjEiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiQWRtaW4iLCJBZG1pbiI6InRydWUiLCJleHAiOjE3NDc5Nzk5MjZ9.H4vTjFfbSNFHA86repUUOVZhu6gDHQNcAOAGsOatQLw";
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjEiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiQWRtaW4iLCJBZG1pbiI6InRydWUiLCJleHAiOjE3NDgwNTAwMjV9.W9bRi5DY9DOCUmIVpKqYHGaGdfUnd-ZkLuk80ttFMmQ";
   const lista = document.getElementById("listaUsuarios");
   lista.innerHTML = "";
 
@@ -78,7 +78,7 @@ async function mostrarLibros() {
   lista.innerHTML = "";
 
   //const token = localStorage.getItem("token"); // Asegúrate de haber guardado el token después del login
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjEiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiQWRtaW4iLCJBZG1pbiI6InRydWUiLCJleHAiOjE3NDc5Nzk5MjZ9.H4vTjFfbSNFHA86repUUOVZhu6gDHQNcAOAGsOatQLw";
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjEiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiQWRtaW4iLCJBZG1pbiI6InRydWUiLCJleHAiOjE3NDgwNTAwMjV9.W9bRi5DY9DOCUmIVpKqYHGaGdfUnd-ZkLuk80ttFMmQ";
 console.log(token) 
 try {
     const response = await fetch("https://localhost:44352/api/Books", {
@@ -130,7 +130,7 @@ document.getElementById("formUsuario").addEventListener("submit", async function
   const activo = true;  // Puedes ajustar si quieres poner un checkbox para activarlo
 
   //const token = localStorage.getItem("jwtToken");
-  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjEiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiQWRtaW4iLCJBZG1pbiI6InRydWUiLCJleHAiOjE3NDc5Nzk5MjZ9.H4vTjFfbSNFHA86repUUOVZhu6gDHQNcAOAGsOatQLw";
+  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjEiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiQWRtaW4iLCJBZG1pbiI6InRydWUiLCJleHAiOjE3NDgwNTAwMjV9.W9bRi5DY9DOCUmIVpKqYHGaGdfUnd-ZkLuk80ttFMmQ";
   const mensaje = document.getElementById("mensajeUsuario");
 
   if (!token) {
@@ -185,8 +185,7 @@ document.getElementById("formLibro").addEventListener("submit", async function (
   const activo = document.getElementById("activoLibro").value === "true";
 
   //const token = localStorage.getItem("jwtToken");
-  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjEiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiQWRtaW4iLCJBZG1pbiI6InRydWUiLCJleHAiOjE3NDc5Nzk5MjZ9.H4vTjFfbSNFHA86repUUOVZhu6gDHQNcAOAGsOatQLw";
-  const mensaje = document.getElementById("mensajeLibro");
+  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjEiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiQWRtaW4iLCJBZG1pbiI6InRydWUiLCJleHAiOjE3NDgwNTAwMjV9.W9bRi5DY9DOCUmIVpKqYHGaGdfUnd-ZkLuk80ttFMmQ"
 
   if (!token) {
     mensaje.textContent = "No estás autenticado.";
@@ -248,10 +247,80 @@ document.getElementById("logoutBtn").addEventListener("click", () => {
   window.location.href = "index.html";
 });
 
+async function cargarLibros() {
+  const select = document.getElementById("libroSelect");
+  select.innerHTML = "<option value=''>Seleccionar libro</option>";
+
+  try {
+   // const token = localStorage.getItem("token");
+  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjEiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiQWRtaW4iLCJBZG1pbiI6InRydWUiLCJleHAiOjE3NDgwNTAwMjV9.W9bRi5DY9DOCUmIVpKqYHGaGdfUnd-ZkLuk80ttFMmQ"
+
+    const response = await fetch("https://localhost:44352/api/Books", {
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`
+      }
+    });
+
+    if (!response.ok) throw new Error("No se pudo cargar libros");
+
+    const libros = await response.json();
+    libros.forEach(libro => {
+      const option = document.createElement("option");
+      option.value = libro.id;
+      option.textContent = libro.titulo;
+      select.appendChild(option);
+    });
+  } catch (error) {
+    console.error("Error al cargar libros:", error);
+    select.innerHTML = "<option value=''>Error al cargar libros</option>";
+  }
+}
+
+// Registrar préstamo
+document.getElementById("formPrestamo").addEventListener("submit",async function (event) {
+  event.preventDefault(); // ← esto evita que el formulario recargue la página
+
+
+  var idLibro = parseInt(document.getElementById("libroSelect").value);
+  var cedulaValor = parseInt(document.getElementById("cedulaUsuarioPrestamo").value);
+  var mensaje = document.getElementById("mensajePrestamo");
+  try {
+   // const token = localStorage.getItem("token");
+   const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjEiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiQWRtaW4iLCJBZG1pbiI6InRydWUiLCJleHAiOjE3NDgwNTAwMjV9.W9bRi5DY9DOCUmIVpKqYHGaGdfUnd-ZkLuk80ttFMmQ"
+
+    const response = await fetch("https://localhost:44352/api/Loans", {
+      method: "POST",
+      headers: {
+        "Authorization": `Bearer ${token}`,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        idLibro: idLibro,
+        cedulaUsuario: cedulaValor
+      })
+    });
+
+    if (!response.ok) throw new Error("Error al registrar préstamo");
+
+    mensaje.textContent = "Préstamo registrado con éxito.";
+    mensaje.style.color = "green";
+    document.getElementById("formPrestamo").reset();
+
+  } catch (error) {
+    mensaje.textContent = "Error: No se pudo registrar el préstamo.";
+    mensaje.style.color = "red";
+    console.error(error);
+  }
+});
+
+
+
 // Inicialización
 mostrarUsuarios();
 mostrarLibros();
 obtenerYMostrarPrestamos();
+cargarLibros();
 
 async function obtenerYMostrarPrestamos() {
   const lista = document.getElementById("listaPrestamos");
@@ -259,7 +328,7 @@ async function obtenerYMostrarPrestamos() {
 
   try {
     //const token = localStorage.getItem("jwtToken");
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjEiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiQWRtaW4iLCJBZG1pbiI6InRydWUiLCJleHAiOjE3NDc5Nzk5MjZ9.H4vTjFfbSNFHA86repUUOVZhu6gDHQNcAOAGsOatQLw";
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjEiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiQWRtaW4iLCJBZG1pbiI6InRydWUiLCJleHAiOjE3NDgwNTAwMjV9.W9bRi5DY9DOCUmIVpKqYHGaGdfUnd-ZkLuk80ttFMmQ";
   
     const response = await fetch("https://localhost:44352/api/Loans", {
       method: "GET",
